48826c914f0d372f1d7bcdf354e1d9b03b3fbbd3
hemkar <kumarhemkar@yahoo.com>
2018-10-16 18:15:30 +0530

    Updated program with debug statements.
    Multi-Threaded program to print EVEN and ODD number using 2 threads.

--- PrintEvenAndOdd2.java
+++ PrintEvenAndOdd2.java
@@ -4,6 +4,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class PrintEvenAndOdd2 {
 	static AtomicInteger ai = new AtomicInteger(1);
+	//static volatile Integer ai=1;//We cant use Volatile as ++ operator need to be atomic so use AtomicInteger
 
 	public static void main(String[] args) throws InterruptedException {
 
@@ -30,10 +31,12 @@ class Even2 extends Thread {
 	public void run() {
 		while (i.get() < 10) {
 			synchronized (object) {
+				System.out.println("Even thread line 34 inside synchronized block");
 				while (i.get() % 2 != 0) {
 					try {
+						System.out.println("Even thread line 37 going to wait because no is odd "+i);
 						object.wait();
-						System.out.println(Thread.currentThread().getName() + " waiting");
+						System.out.println(Thread.currentThread().getName() + " GOT NOTIFICATION");
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
@@ -58,14 +61,17 @@ class Odd2 extends Thread {
 	public void run() {
 		while (i.get() < 10) {
 			synchronized (object) {
+				System.out.println("Odd thread line 63 inside synchronized block");
 				while (i.get() % 2 == 0) {
+					System.out.println("Odd thread line 65 going to wait becaue no is even " +i);
 					try {
 						object.wait();
-						System.out.println(Thread.currentThread().getName() + " waiting");
+						System.out.println(Thread.currentThread().getName() + " GOT NOTIFICATION");
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
 				}
+				System.out.println("Odd thread line 73");
 				System.out.println(Thread.currentThread().getName() + " printed :" + i.getAndIncrement());
 				object.notifyAll();
 			}
