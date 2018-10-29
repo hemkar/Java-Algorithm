c6cb98db736146682576617a032d37f520cbd883
hemkar kumar <kumarhemkar@yahoo.com>
2018-10-29 08:20:13 +0530

    Added comment
    Added comment in merge sort algorithm

--- src/com/test/searchandshort/MergeSortAlgo.java
+++ src/com/test/searchandshort/MergeSortAlgo.java
@@ -5,28 +5,32 @@ import java.util.Arrays;
 public class MergeSortAlgo {
 
 	public static void mergeSort(int arr[]) {
-
-		if (arr.length < 2)
+		//IT IS CONSIDERD THAT ARRAY OF ONLY ELEMENT IS ALREADY SORTED.
+		if (arr.length < 2) //base case:Exit recursion if array has only one element.
 			return;
-
-		int mid = arr.length / 2;
-		int left[] = new int[mid];
-		int right[] = new int[(arr.length) - mid];
-		for (int i = 0; i < left.length; i++) {
+		//Divide and conquer
+		int mid = arr.length / 2; //dive array into half
+		int left[] = new int[mid]; //create an auxilary array to store left half
+		int right[] = new int[(arr.length) - mid];//create an array to store left half
+		for (int i = 0; i < left.length; i++) { //populate elements in left array
 			left[i] = arr[i];
 		}
 		int i = mid;
-		for (int j = 0; j < right.length; j++) {
+		for (int j = 0; j < right.length; j++) {//populate elements in right array
 			right[j] = arr[i++];
 		}
 
-		mergeSort(left);
-		mergeSort(right);
-
-		merge(left, right, arr);
+		mergeSort(left);//call recursively for left half
+		mergeSort(right);//call recursivly for right half
+		
+		//The first call to merge will happen when there is only one element in the left and right sub array. 
+		//An array of one element is already considered sorted.
+		merge(left, right, arr);//Call to merge 
+		
 
 	}
 
+	//This method will merge 2 sorted array.
 	private static void merge(int[] left, int[] right, int[] arr) {
 		
 		int i=0;
@@ -35,7 +39,7 @@ public class MergeSortAlgo {
 		
 		while(i<left.length && j<right.length){// keep comparing and adding elements into main array
 			if(left[i]<=right[j]){//if left array element is smaller
-				arr[k]=left[i]; //add to main array is increment the index
+				arr[k]=left[i]; //add to main array and increment the index
 				i++; //increment index of left array not right array
 				k++;//increment index of main array to store next smallest element
 			}else{ 	//if right array element is smaller.
