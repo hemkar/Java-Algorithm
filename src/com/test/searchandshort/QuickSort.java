11fd9273d072a955d397c2506a77f5cc48523bc2
hemkar kumar <kumarhemkar@yahoo.com>
2018-10-29 08:34:16 +0530

    Update QuickSort.java

--- src/com/test/searchandshort/QuickSort.java
+++ src/com/test/searchandshort/QuickSort.java
@@ -2,29 +2,33 @@ package com.test.sorting;
 
 
 import java.util.Arrays;
-
+// quicksort is divive and conquer algo.
+// it is not stable sorting algo.
 public class QuickSort {
 
 	public static void quickSort(int arr[], int start, int end) {
 
 		if (start < end) { //do this only if start index is less than end
 			int partitionIndex = partition(arr, start, end); //call partition function to get partition so that all element less than partition are to the left
-																// and larger are at the right.
+									// and larger are at the right.
 			quickSort(arr, start, partitionIndex - 1);// call quickSort on  left partition
 			quickSort(arr, partitionIndex + 1, end); // call quickSort on right partition
 		}
 	}
 
+	//sort array in such a way that all elemet lesser than pivot is at the left(not necessarily in order ) and
+	//All element greater than pivot are at the right side (not necessarily in order ) of pivot. 
 	public static int partition(int arr[], int start, int end) {
-
+		//we can take pivot as end index.
 		int pivot = end;
+		//Assume all elements are greater than element at index 0
 		int pindex = 0;
 
 		for (int i = 0; i < end; i++) {
 			if (arr[i] < arr[pivot]) { //swap number at index i with partition index if it is smaller than pivot.
 				int temp = arr[i];
 				arr[i] = arr[pindex];
-				pindex++;
+				pindex++; //increment partition index
 			}
 		}
 
