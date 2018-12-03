81f07d0e5fbee7cccf34492afc9f2e7365ea686a
hemkar <kumarhemkar@yahoo.com>
2018-12-03 07:33:23 +0530

    Dynamic programming stairs problem

--- src/com/test/dynamicprogramming/Chapter_8_Q1_TripleStep.java
+++ src/com/test/dynamicprogramming/Chapter_8_Q1_TripleStep.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * A child running up a staircase with n steps and can hop either 1 step , 2 step , 3 steps at a time.
  * Implement a method to count how many possible ways the child can up the stairs.
  * 
- * Note: Below solution is generic we can use any number of steps and any set of allowed steps
+ * Note: Below solution is generic we can use any number of steps and any set of allowed steps.
  * 
  * */
 public class Chapter_8_Q1_TripleStep {
@@ -31,16 +31,17 @@ public class Chapter_8_Q1_TripleStep {
 	}
 
 	private static void FindCombination(int n, int[] arr2) {
+		
 		int totalSteps = n + 1;
 		int ValidStepsArray = arr2.length;
-		int[][] arr = new int[ValidStepsArray+1][n + 1];
-
+		int[][] arr = new int[ValidStepsArray + 1][n + 1];
+		
 		for (int i = 0; i < (ValidStepsArray + 1); i++) {
 			arr[i][0] = 1;
 		}
 
-		 System.out.println(Arrays.deepToString(arr));
-		for (int i = 1; i < (ValidStepsArray+1); i++) {
+		//System.out.println(Arrays.deepToString(arr)); to print the array after initialization
+		for (int i = 1; i < (ValidStepsArray + 1); i++) {
 			for (int sum = 1; sum < totalSteps; sum++) {
 				if (i > sum) {
 					System.out.println("in side IF " + "Coin " + i + " Sum " + sum);
@@ -55,10 +56,9 @@ public class Chapter_8_Q1_TripleStep {
 			}
 
 		}
-		
-		System.out.println("Number of possible way child can go up the stairs of size " +n +" is "+ arr[ValidStepsArray][totalSteps-1]);
-	} 
-	
-	
+
+		System.out.println("Number of possible way child can go up the stairs of size " + n + " is "
+				+ arr[ValidStepsArray][totalSteps - 1]);
+	}
 
 }
